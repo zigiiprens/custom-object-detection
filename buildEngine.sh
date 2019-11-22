92759020a4a11d0ccf9f1822fe907d6191bdd198
Samir <samosyzigii1@gmail.com>
2019-11-22 18:15:02 +0300

    updated with jetsonNano
    jetsonNano dev with <runneronjetsonNano.py>

---
+++ buildEngine.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 buildEngine.py ${trained-inference/output_inference_graph_v1_faces/trt/saved_model.pb}
--- cameraTFTRT.sh
+++ cameraTFTRT.sh
@@ -2,4 +2,4 @@
 
 #python3 cameraTFTRT.py --webcam --model ssd_mobilenet_v2_quantized_300x300_coco --num-classes 1 --build
 
-python3 cameraTFTRT.py --rtsp --uri fake --model ssd_mobilenet_v2_quantized_300x300_coco --num-classes 1 --build
+python3 cameraTFTRT.py --rtsp --uri fake --model ssd_mobilenet_v2_quantized_300x300_coco --num-classes 1 
